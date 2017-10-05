8150079c69f7bf8e045f621e186ad8e7496eaa2e
Terence Lee <hone02@gmail.com>
2017-10-05 02:18:04 -0500

    add more tests

--- spec/docopt_spec.rb
+++ spec/docopt_spec.rb
@@ -30,4 +30,64 @@ describe "Docopt" do
     expect(options.get_bool("--help")).to be true
     expect(options["--help"]).to be true
   end
+
+  it "should parse strings" do
+    x    = "1"
+    y    = "2"
+    argv = "naval_fate ship shoot #{x} #{y}".split
+
+    options = Docopt.parse(USAGE, argv)
+    expect(options["ship"]).to eq(true)
+    expect(options["shoot"]).to eq(true)
+    expect(options["<x>"]).to eq(x)
+    expect(options["<y>"]).to eq(y)
+  end
+
+  it "should set short and long options are when provided" do
+    argv = "naval_fate -o".split
+    options = Docopt.parse(USAGE, argv)
+    expect(options["--option"]).to eq(true)
+    argv = "naval_fate --option".split
+    options = Docopt.parse(USAGE, argv)
+    expect(options["--option"]).to eq(true)
+  end
+
+  it "should parse an array of strings" do
+    names = %w(enterprise mission)
+    argv  = "naval_fate ship new #{names.join(" ")}".split
+
+    options = Docopt.parse(USAGE, argv)
+    expect(options["ship"]).to eq(true)
+    expect(options["<name>"]).to eq(names)
+  end
+
+  it "should use default values" do
+    argv = "naval_fate ship foo move 0 0".split
+
+    options = Docopt.parse(USAGE, argv)
+    expect(options["--speed"]).to eq("10")
+  end
+
+  it "should return nil for unused options" do
+    argv = "naval_fate ship foo move 0 0".split
+
+    options = Docopt.parse(USAGE, argv)
+    expect(options["--acc"]).to be_nil
+  end
+
+  it "should handle complex cases" do
+    name  = "enterprise"
+    x     = "1"
+    y     = "2"
+    speed = "10"
+    argv  = "naval_fate ship #{name} move #{x} #{y} --speed=#{speed}".split
+
+    options = Docopt.parse(USAGE, argv)
+    expect(options["ship"]).to eq(true)
+    expect(options["move"]).to eq(true)
+    expect(options["<name>"]).to eq([name])
+    expect(options["<x>"]).to eq(x)
+    expect(options["<y>"]).to eq(y)
+    expect(options["--speed"]).to eq(speed)
+  end
 end
